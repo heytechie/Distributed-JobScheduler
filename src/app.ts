c920a6776e6c28125daf9560a84f3b06e3a55986
heytechie <tanishk.kapoorpvt@gmail.com>
2026-09-02 01:31:33 +0530

    Jobs creation done at initial stage-scaling of infra is future perspective

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,17 @@
 import express from "express";
-
+import { requestLogger } from "./middleware/request-logger.middleware.js";
+import { errorHandler } from "./middleware/error.middleware.js";
+import jobRoutes from '../src/modules/jobs/job.route.js'
 const app = express();
 
+app.use(requestLogger);
 app.use(express.json());
 
-app.get("/healthy", (req, res) => {
+app.get("/health", (req, res) => {
   res.json({ status: "ok" });
 });
 
+app.use("/jobs",jobRoutes);
+app.use(errorHandler);
 
 export default app;
